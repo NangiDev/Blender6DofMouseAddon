6d442f230498578e68f543b75f45b354a1c2fbf2
Joel Setterberg <NangiDev@users.noreply.github.com>
2023-02-19 18:08:55 +0100

    finally something

--- viewport_control.py
+++ viewport_control.py
@@ -19,36 +19,25 @@ bl_info = {
 
 
 class MyThread(threading.Thread):
-    def __init__(self, context, queue):
+    def __init__(self, queue, port):
         threading.Thread.__init__(self)
         self.daemon = True
-        self.context = context
-        self.queue = queue
+        self._queue = queue
         self._is_running = False
-        self.ser = None
-        try:
-            self.ser = serial.Serial(port='COM10', baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
-                                     stopbits=serial.STOPBITS_ONE, timeout=0.01)
-            self.ser.close()
-            self.ser.open()
-        except:
-            print("Ser could not connect")
+        self._is_reading = False
 
     def run(self):
-        if not self.ser == None:
-            self.ser.flush()
-            while self._is_running and self.queue.qsize() < 6:
-                data = self.ser.read(2)
-                self.ser.read(2)
-                result = int.from_bytes(data, byteorder='little')
-                print(result)
-                self.queue.put(result)
-                self._is_running = self.queue.qsize() < 6
+        while self._is_running and self._is_reading:
+            self._queue.put(2)
+            print("Back Thread: {}".format(self._queue.qsize()))
+            self._is_reading = self._queue.qsize() < 6
 
 
 class MyAddonProperties(PropertyGroup):
     def update_is_running(self, context):
         self.read_tread._is_running = self.is_running
+        self.read_tread._is_reading = self.is_running
+        self.queue.queue.clear()
         self.read_tread.run()
 
     def port_callback_list(self, context):
@@ -56,8 +45,7 @@ class MyAddonProperties(PropertyGroup):
                  for item in list(serial.tools.list_ports.comports())]
         return ports if ports else (('-1', 'No device found', ''),)
 
-    is_running: BoolProperty(
-        name="_is_running", default=False, update=update_is_running)
+    is_running: BoolProperty(default=False, update=update_is_running)
 
     port_dropdown_list: EnumProperty(
         name="Port",
@@ -65,7 +53,7 @@ class MyAddonProperties(PropertyGroup):
     )
 
     queue = queue.Queue()
-    read_tread = MyThread(context=bpy.context, queue=queue)
+    read_tread = MyThread(queue=queue, port='COM10')
     read_tread.start()
 
 
@@ -90,11 +78,27 @@ class SIX_DOF_PT_main_panel(Panel):
         row.prop(properties, "port_dropdown_list")
         row.enabled = not properties.is_running and properties.port_dropdown_list != '-1'
 
-        if properties.queue.qsize() > 0:
-            print(properties.queue.get())
+
+class OT_fetch_data_operator(Operator):
+    bl_label = "Fetch USB data"
+    bl_idname = "object.fetch_usb_data"
+
+    def execute(self, context):
+        properties = context.scene.my_addon
+        while (not properties.read_tread._is_reading) and properties.is_running:
+            print("Main Thread: {}".format(properties.queue.get()))
+            properties.read_tread._is_reading = properties.queue.qsize() <= 0
+        if properties.read_tread._is_reading:
+            properties.read_tread.run()
+        return {'FINISHED'}
 
 
-classes = [MyAddonProperties, SIX_DOF_PT_main_panel]
+classes = [MyAddonProperties, SIX_DOF_PT_main_panel, OT_fetch_data_operator]
+
+
+def timer_call_fnc():
+    bpy.ops.object.fetch_usb_data()
+    return 0.01
 
 
 def register():
@@ -102,14 +106,14 @@ def register():
         bpy.utils.register_class(cls)
     bpy.types.Scene.my_addon = PointerProperty(
         type=MyAddonProperties)
+    bpy.app.timers.register(timer_call_fnc)
 
 
 def unregister():
-    if not bpy.context.scene.my_addon.read_tread.ser == None:
-        bpy.context.scene.my_addon.read_tread.ser.close()
     del bpy.types.Scene.my_addon
     for cls in classes:
         bpy.utils.unregister_class(cls)
+    bpy.app.timers.unregister(timer_call_fnc)
 
 
 if __name__ == "__main__":
